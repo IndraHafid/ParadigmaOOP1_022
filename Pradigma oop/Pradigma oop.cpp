305d525f2659f283c2e6aca069ed49b2804fe64f
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-21 15:00:08 +0700

    membuat fungsi utama dengan pembuatan objek mahasiswa

--- Pradigma oop/Pradigma oop.cpp
+++ Pradigma oop/Pradigma oop.cpp
@@ -25,10 +25,13 @@ public:
     }
 };
 
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    mahasiswa mhs(12, "Asroni", 90.5);
+        return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
